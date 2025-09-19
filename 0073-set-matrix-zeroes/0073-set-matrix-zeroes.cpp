a08342df9035c84353dd5f1203db7015a675e0a3
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2025-09-19 16:16:27 +0530

    Time: 0 ms (100%), Space: 18.8 MB (11.03%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-    int m = matrix.size(), n = matrix[0].size();
-    bool firstRowZero = false;
-
-    for (int j = 0; j < n; ++j) {
-        if (matrix[0][j] == 0) {
-            firstRowZero = true;
-            break;
-        }
-    }
-
-    for (int i = 1; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        queue<pair<int,int>> q;
+        for (int i{0}; i<n ; i++){
+            for (int j{0}; j<m; j++){
+                if (matrix[i][j] == 0){
+                    q.push({i,j});
+                }
             }
         }
-    }
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+                
+            for (int i{0}; i<n; i++){
+                for (int j{0}; j<m; j++){
+                    if (i == r || j==c){
+                        matrix[i][j] = 0;
+                    }
+                
+                }
 
-    for (int i = 1; i < m; ++i) {
-        for (int j = 1; j < n; ++j) {
-            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                matrix[i][j] = 0;
             }
+            q.pop();
         }
-    }
-
-    if (matrix[0][0] == 0) { 
-        for (int i = 0; i < m; ++i) {
-            matrix[i][0] = 0;
-        }
-    }
-
-    if (firstRowZero) {
-        for (int j = 0; j < n; ++j) {
-            matrix[0][j] = 0;
-        }
-    }
     }
 };
