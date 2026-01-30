a451d6c3091d8d126f52ec50689c20b024cab85d
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-01-30 18:59:04 +0530

    Time: 7 ms (51.17%), Space: 13.4 MB (36.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int char2num(char a) {
-        switch (a) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
-    }
-
     int romanToInt(string s) {
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i + 1 < s.length() && char2num(s[i]) < char2num(s[i + 1])) {
-                result -= char2num(s[i]);
+        unordered_map<char,int> lst = { {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int res{0};
+        for (int i{0}; i<s.size()-1;i++){
+            if (lst[s[i]] < lst[s[i + 1]]) {
+                res -= lst[s[i]];
             } else {
-                result += char2num(s[i]);
+                res += lst[s[i]];
             }
+
         }
-        return result;
+        return res + lst[s.back()];//because we didn't take the last value in the loop
     }
 };
