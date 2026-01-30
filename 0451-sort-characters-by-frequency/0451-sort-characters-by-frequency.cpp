504cd2e42d66f599c465e03373603a434807c822
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-01-30 09:22:51 +0530

    Time: 7 ms (21.98%), Space: 17.2 MB (7.95%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -6,16 +6,14 @@ public:
         for(auto i:s){
             mpp[i]++;
         }
-        vector<pair<char, int>> freqArr;
-        for (auto [chr, freq]:mpp) freqArr.push_back({chr,freq});
+        vector<vector<char>> bucket(s.length()+1);
+        for (auto [chr, freq]:mpp) bucket[freq].push_back(chr);
         
-        auto cmp = [&](pair<char, int>& a, pair<char,int>& b){
-            return a.second> b.second;
-        };
-        sort(freqArr.begin(),freqArr.end(), cmp);
-
-        for (auto [ch,fq]:freqArr){
-            res.append(fq,ch);
+        for (int fq = s.length(); fq>=1; fq--){
+            for (auto ch: bucket[fq]){
+                res.append(fq, ch);
+            }
+            
         }
         return res;
     }
