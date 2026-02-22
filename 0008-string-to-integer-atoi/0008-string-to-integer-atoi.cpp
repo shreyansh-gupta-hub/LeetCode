6a6769811ba06e098efed4ac762a47deaabb4607
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-22 17:54:09 +0530

    Time: 4 ms (6.11%), Space: 9.3 MB (21.7%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
+    //recursive implementation
+    int helper(const string &s, int i, long long num, int sign) {
+    if (i >= s.size() || !isdigit(s[i]))
+        return (int)(sign * num);
+    num = num * 10 + (s[i] - '0');
+    if (sign * num <= INT_MIN) return INT_MIN;
+    if (sign * num >= INT_MAX) return INT_MAX;
+    return helper(s, i + 1, num, sign);
+}
     int myAtoi(string s) {
         int i = 0, sign = 1;
         long res = 0;
-        // Skip leading whitespaces
         while (i < s.size() && s[i] == ' ') i++;
-        // Return 0 if only spaces are found
         if (i == s.size()) return 0;
-        // Check for optional '+' or '-' sign
         if (s[i] == '-') {
             sign = -1;
             i++;
         } else if (s[i] == '+') {
             i++;
         }
-        // Convert characters to integer while valid digits
-        while (i < s.size() && isdigit(s[i])) {
-            res = res * 10 + (s[i] - '0');
-            // Clamp to INT_MAX if overflow occurs
-            if (sign * res > INT_MAX) return INT_MAX;
-            // Clamp to INT_MIN if underflow occurs
-            if (sign * res < INT_MIN) return INT_MIN;
-            i++;
-        }
-
-        // Return final result after applying sign
-        return (int)(sign * res);
+        return helper(s, i, 0, sign);
     }
 
 };
