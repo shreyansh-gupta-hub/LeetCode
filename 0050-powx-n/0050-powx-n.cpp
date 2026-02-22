fe6d714a97c509f1e567d287234b352e1d4e7b95
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-22 18:40:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
+    //recursive implementation
     double myPow(double x, int n) {
-        if(n==0) return 1.0;
-        if(x==0) return 0.0;
-        if(x==1) return 1.0;
-        if(x==-1 && n%2==0) return 1.0;
-        if(x==-1 && n%2!=0) return -1.0;
-       long binForm=n;
-       if(n<0){
-        x=1/x;
-        binForm = -binForm;
-       }
-       double ans = 1;
-       while(binForm > 0){
-        if(binForm % 2 == 1){
-            ans *= x;
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        x *= x;
-        binForm /= 2;
-       }
-       return ans;
+        return fastPow(x, N);
+    }
+
+    double fastPow(double x, long long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) return half * half;
+        else return half * half * x;
     }
 };
