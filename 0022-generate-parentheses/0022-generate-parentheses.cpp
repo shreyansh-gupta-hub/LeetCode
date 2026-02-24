76392f9d6a83bf9f937301694e09c7ed7c03de20
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-02-24 14:16:19 +0530

    Time: 2 ms (81.87%), Space: 15.8 MB (39.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-
-  void par(char op,char cl,int op_cnt,int cl_cnt,vector<string>&ans,string x,int n){
-
-    if(cl_cnt==n&&op_cnt==n){
-      ans.push_back(x);
-      return;
+    void solve(int open,int close, string s, vector<string>& ans){
+        if(open == 0 && close ==0){
+            ans.push_back(s);
+            return;
+        }
+        if(open>0) solve(open-1, close, s +"(",ans);
+        if(close>open) solve(open,close-1,s+")",ans);
     }
-    if(op_cnt<n){
-      x.push_back(op);
-      par(op,cl,op_cnt+1,cl_cnt,ans,x,n);
-      x.pop_back();
-    }
-
-    if(cl_cnt<op_cnt){
-      x.push_back(cl);
-      par(op,cl,op_cnt,cl_cnt+1,ans,x,n);
-      x.pop_back();
-    }
-
-  }
-
     vector<string> generateParenthesis(int n) {
-        char op='(';
-        char cl=')';
-        int op_cnt=0;
-        int cl_cnt=0;
-        vector<string>ans;
-        string x;
-
-        par(op,cl,op_cnt,cl_cnt,ans,x,n);
-
+        vector<string> ans;
+        solve(n,n,"",ans);
         return ans;
     }
 };
