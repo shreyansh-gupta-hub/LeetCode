8b7f290bc22bd7661dd041b38352df963f81b655
Shreyansh Gupta <shreyanshguptasprint@gmail.com>
2026-04-12 20:51:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (88.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,25 @@
 class Solution {
-public:
-    
-    void comb(vector<string> st,int index,string digits,string test,vector<string>& result){
-        if(index==digits.length()){
-            result.push_back(test);
+private:
+    void solve(string digits, string output, int index, vector<string>& ans, string mapping[]){
+        if(index >= digits.length()){
+            ans.push_back(output);
             return;
         }
-        char char1 = digits[index];
-        int ind=(char1-'0')-1;
-        string temp = st[ind],temp2 = test;
-        for(int i=0;i<temp.length();i++){
-            test+=temp[i];
-            comb(st,index+1,digits,test,result);
-            test=temp2;
+        int number = digits[index] - '0';
+        string value = mapping[number];
+        for (int i{0}; i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digits, output,index+1,ans,mapping);
+            output.pop_back();
         }
-        return;
     }
+public:
     vector<string> letterCombinations(string digits) {
-        vector<string> st= {"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string> result;
-        if(digits==""){
-            return result;
-        }
-        comb(st,0,digits,"",result);
-        return result;
+        vector<string> ans;
+        string output;
+        int index{0};
+        string mapping[10] = {"", "", "abc", "def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output,index, ans, mapping);
+        return ans;
     }
 };
